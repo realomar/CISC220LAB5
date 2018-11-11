9fa4c2868306c1f50b6298c36e957c06a4e0e79f
Omar Ahmad <eatmorerice@outlook.com>
2018-11-11 17:35:01 -0500

    CLion stuff

---
+++ cmake-build-debug/CMakeFiles/CISC220LAB5.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CISC220LAB5.dir/main.cpp.obj"
+  "CISC220LAB5.pdb"
+  "CISC220LAB5.exe"
+  "CISC220LAB5.exe.manifest"
+  "libCISC220LAB5.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CISC220LAB5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
